7c8260ca6ca8fca3fa51309a278395207193745e
Olivier Barais <barais@irisa.fr>
2018-04-22 17:55:18 +0200

    new

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ git clone -b maint https://bitbucket.org/petsc/petsc petsc
 cd petsc
 git checkout 73804e8 # pour revenir à la version de 2016
 ./configure --with-cc=gcc --with-cxx=g++ --with-fc=gfortran --download-fblaslapack --with-mpi=0  --with-debugging=0 PETSC_ARCH=linux-gnu-opt
-make PETSC_DIR=/home/barais/git/crunchflow/crunchtope/petsc PETSC_ARCH=linux-gnu-opt all
-make PETSC_DIR=/home/barais/git/crunchflow/crunchtope/petsc PETSC_ARCH=linux-gnu-opt check
-make PETSC_DIR=/home/barais/git/crunchflow/crunchtope/petsc PETSC_ARCH=linux-gnu-opt streams
+make PETSC_DIR=`pwd` PETSC_ARCH=linux-gnu-opt all
+make PETSC_DIR=`pwd` PETSC_ARCH=linux-gnu-opt check
+make PETSC_DIR=`pwd` PETSC_ARCH=linux-gnu-opt streams
 cd ..
 make
 
