7fa09c529cd40906580a79ef6fe08bcba1fa3e87
Olivier Barais <barais@irisa.fr>
2018-04-22 17:53:51 +0200

    new

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ hg clone https://bitbucket.org/crunchflow/crunchtope
 cd crunchtope/
 sudo apt-get install -y gfortran 
 git clone -b maint https://bitbucket.org/petsc/petsc petsc
+cd petsc
 git checkout 73804e8 # pour revenir à la version de 2016
 ./configure --with-cc=gcc --with-cxx=g++ --with-fc=gfortran --download-fblaslapack --with-mpi=0  --with-debugging=0 PETSC_ARCH=linux-gnu-opt
 make PETSC_DIR=/home/barais/git/crunchflow/crunchtope/petsc PETSC_ARCH=linux-gnu-opt all
