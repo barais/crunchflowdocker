6dd4257b3ebfe0d5e253da98191ba7facc9ec734
Olivier Barais <barais@irisa.fr>
2018-04-25 18:46:59 +0200

    update Readme

--- README.md
+++ README.md
@@ -13,3 +13,10 @@ To run the docker
 ```bash
 docker run -t -i -v "YOURFOLDERWITHDATA":/data -w /data barais/crunchtop 
 ```
+
+
+This repository also share some script files to automate the running of tasks on top of docker swarm using 
+[jaas](https://github.com/alexellis/jaas) and parallels [1]
+
+[1] O. Tange (2011): GNU Parallel - The Command-Line Power Tool ;login: The USENIX Magazine, February 2011:42-47.
+
--- executor.sh
+++ executor.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 export diffusion=$1
 export dependence=$2
-docker run -t -v /var/run/docker.sock:/var/run/docker.sock alexellis2/jaas run -r -t=1000s --env RES=res_dif${diffusion}_dep${dependence} --env DIFFUSION=${diffusion} --env DEPENDANCE=${dependence}   -m /opt/data/crunchflowdata=/data -i barais/crunchtop -d "sh /data/execute.sh"
+docker run -t -v /var/run/docker.sock:/var/run/docker.sock alexellis2/jaas run \
+  -r -t=1000s --env RES=res_dif${diffusion}_dep${dependence} --env DIFFUSION=${diffusion} --env DEPENDANCE=${dependence} \
+   -m /opt/data/crunchflowdata=/data -i barais/crunchtop -d "sh /data/execute.sh"
  
